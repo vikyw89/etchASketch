37a25263c482c5e169828672692aaa24f400bda8
vikyw89 <vikyw89@gmail.com>
2022-11-09 19:33:27 +0800

    refactoring updating mouseDown

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let activeColor = "white"
 let activeSize = 16
 
+let mouseDown = false
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
 const editPixel = (e) => {
-    console.log(e)
+    if (e.type === 'mouseover' && !mouseDown) return
     e.target.setAttribute('style', `background-color:${activeColor};`)
 }
 
@@ -12,6 +16,7 @@ const generateGrid = (activeSize) => {
     for (let i = 1; i <= activeSize*activeSize; i++) {
         const content = document.createElement('div')
         content.setAttribute('class', 'gridContent')
+        content.addEventListener('mousedown', editPixel)
         content.addEventListener('mouseover', editPixel)
         grid.appendChild(content)
     }
@@ -20,15 +25,18 @@ const generateGrid = (activeSize) => {
 }
 
 const resizeGrid = (e) => {
-    activeSize = e.target.value
-    if (activeSize > 100 ) {
-        activeSize = 100
-    }
+    e.target.value > 100
+        ? activeSize = 100
+        : activeSize = e.target.value
     generateGrid(activeSize)
 }
 
 const setActiveColor = (e) => {
     activeColor = e.target.value
+    const hover = document
+        .querySelector('.gridContent:hover')
+        .setAttribute('style',`background-color:${activeColor};`)
+    console.log(hover)
 }
 
 const colorPicker = document.querySelector('#colorPicker')

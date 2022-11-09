cc9d92d451db69b8c7de064e565d1403ea51a0c3
vikyw89 <vikyw89@gmail.com>
2022-11-09 19:07:36 +0800

    further refactoring

--- app.js
+++ app.js
@@ -1,55 +1,41 @@
-const grid = document.querySelector('.gridContainer')
 let activeColor = "white"
 let activeSize = 16
 
-const brush = (e) => {
+const editPixel = (e) => {
     console.log(e)
     e.target.setAttribute('style', `background-color:${activeColor};`)
 }
 
-const responsivePixels = () => {
-    const pixels = document.querySelectorAll('.gridContent')
-    pixels.forEach(pixel => {
-        pixel.addEventListener('click', brush)
-    })
-}
-
 const generateGrid = (activeSize) => {
+    const grid = document.querySelector('.gridContainer')
+    grid.innerHTML = ''
     for (let i = 1; i <= activeSize*activeSize; i++) {
         const content = document.createElement('div')
         content.setAttribute('class', 'gridContent')
+        content.addEventListener('mouseover', editPixel)
         grid.appendChild(content)
     }
-    grid.setAttribute('style',`grid-template-columns: repeat(${activeSize}, 1fr); grid-template-rows: repeat(${activeSize}, 1fr);`)
-    responsivePixels()
+    grid.setAttribute('style',`grid-template-rows: repeat(${activeSize}, 1fr);`)
+    grid.setAttribute('style',`grid-template-columns: repeat(${activeSize}, 1fr);`)
 }
 
-const removeGrid = () => {
-    document
-        .querySelectorAll('.gridContent')
-        .forEach(el=> el.remove())
-}
-
-const reworkGrid = (e) => {
+const resizeGrid = (e) => {
     activeSize = e.target.value
     if (activeSize > 100 ) {
         activeSize = 100
     }
-    removeGrid()
     generateGrid(activeSize)
 }
-
 
 const setActiveColor = (e) => {
     activeColor = e.target.value
 }
 
 const colorPicker = document.querySelector('#colorPicker')
-colorPicker.addEventListener('change', setActiveColor)
-
+colorPicker.addEventListener('input', setActiveColor)
 
 const sizePicker = document.querySelector('#sizePicker')
-sizePicker.addEventListener('change', reworkGrid)
+sizePicker.addEventListener('input', resizeGrid)
 
 generateGrid(activeSize)
 

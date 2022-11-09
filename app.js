a4a7d9051e37948e86bb340d96413e8851085d9a
vikyw89 <vikyw89@gmail.com>
2022-11-09 13:58:13 +0800

    grid positioning responsive placeholder for tools

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const grid = document.querySelector('.gridContainer')
 for (let i = 1; i <= 16*16; i++) {
     const content = document.createElement('div')
     content.setAttribute('class', 'gridContent')
+    content.textContent = i
     grid.appendChild(content)
 }
 
